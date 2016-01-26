324eefd09472b9491963e6018c2dc6a97c852e53
rodrigo <rodrigorozendobastos@gmail.com>
2016-01-26 02:53:54 -0300

    Use Regex find cue-phrases in documents

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 __author__ = 'rodrigo'
 
-# In general, the sentences started by ‘‘in summary’’, ‘‘in conclusion’’, ‘‘our investigation’’, ‘‘the paper describes’’ and emphasizes
-# such as ‘‘the best’’, ‘‘the most important’’, ‘‘according to the study’’,
-# ‘‘significantly’’, ‘‘important’’, ‘‘in particular’’, ‘‘hardly’’, ‘‘impossible’’
+import re
 
 expressions = [
     'in summary',
@@ -17,3 +15,21 @@ expressions = [
     'hardly',
     'impossible'
 ]
+
+# CP = Cue-phrase score,
+# CPS = Number of cue-phrases in the sentence,
+# CPD = Total number of cue-phrases in the document
+
+file = open('documents/9150')
+sentences = file.readlines()
+#print(sentences)
+
+x = 'Meu nome é rodrigo bastos. Eu sou louco por computação. Meu nome Rodrigo Bastos'
+
+exactMatch = re.compile('rodrigo bastos', flags=re.IGNORECASE)
+print(exactMatch.findall(x))
+
+# for sentence in sentences:
+#     for expression in expressions:
+#         exactMatch = re.compile(expression, flags=re.IGNORECASE)
+#         print(exactMatch.search(sentence))
