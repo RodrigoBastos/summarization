4b98cd1b56985ea9856576b688029443ea5611fd
rodrigo <rodrigorozendobastos@gmail.com>
2016-01-26 02:36:24 -0300

    domain-specific bonus phrases terms can be good indicators of significant content of a text document

---
+++ main.py
@@ -0,0 +1,19 @@
+__author__ = 'rodrigo'
+
+# In general, the sentences started by ‘‘in summary’’, ‘‘in conclusion’’, ‘‘our investigation’’, ‘‘the paper describes’’ and emphasizes
+# such as ‘‘the best’’, ‘‘the most important’’, ‘‘according to the study’’,
+# ‘‘significantly’’, ‘‘important’’, ‘‘in particular’’, ‘‘hardly’’, ‘‘impossible’’
+
+expressions = [
+    'in summary',
+    'in conclusion',
+    'our investigation',
+    'the paper describes',
+    'the most important',
+    'according to the study',
+    'significantly',
+    'important',
+    'in particular',
+    'hardly',
+    'impossible'
+]
