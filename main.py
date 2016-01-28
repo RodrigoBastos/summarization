52350ce1e25785ffda011e9350d99bee15aa3652
rodrigo <rodrigorozendobastos@gmail.com>
2016-01-28 01:17:31 -0300

    reading all files in directory

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 __author__ = 'rodrigo'
 
 import re
+from os import listdir
+from os.path import isfile, join
+
+onlyfiles = [f for f in listdir('documents') if isfile(join('documents', f))]
+print(onlyfiles)
 
 cue_phrases = [
     'in summary',
@@ -21,28 +26,31 @@ cue_phrases = [
 # CP = Cue-phrase score,
 # CPS = Number of cue-phrases in the sentence,
 # CPD = Total number of cue-phrases in the document
-
-file = open('documents/9136')
-sentences = file.readlines()
+files = []
+for name_file in onlyfiles:
+    file = open('documents/'+name_file)
+    files.append(file.readlines())
 
 # x = 'Meu nome é rodrigo bastos. Eu sou louco por computação. Meu nome Rodrigo Bastos'
 #
 # exactMatch = re.compile('rodrigo bastos', flags=re.IGNORECASE)
 # print(exactMatch.findall(x))
 
-count_cue_phrases = 0
-count_cue_phrases_document = {}
-for sentence in sentences:
-    n_cue_phrases = 0
-    for cue_phrase in cue_phrases:
-        exactMatch = re.compile(cue_phrase, flags=re.IGNORECASE)
-        match = exactMatch.findall(sentence)
-        n_cue_phrases += len(match)
-        count_cue_phrases += len(match)
-    if n_cue_phrases > 0:
-        count_cue_phrases_document[sentence] = n_cue_phrases
-#print(count_cue_phrases_document)
 
-for item in count_cue_phrases_document:
-    print(item, count_cue_phrases_document[item])
+for sentences in files:
+    count_cue_phrases = 0
+    count_cue_phrases_document = {}
+    for sentence in sentences:
+        n_cue_phrases = 0
+        for cue_phrase in cue_phrases:
+            exactMatch = re.compile(cue_phrase, flags=re.IGNORECASE)
+            match = exactMatch.findall(sentence)
+            n_cue_phrases += len(match)
+            count_cue_phrases += len(match)
+        if n_cue_phrases > 0:
+            count_cue_phrases_document[sentence] = n_cue_phrases
+    for item in count_cue_phrases_document:
+        print(item, count_cue_phrases_document[item])
+
+
 
