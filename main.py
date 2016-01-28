e4fbc9432de99d4da1afef06fcd6a6726bdef767
rodrigo <rodrigorozendobastos@gmail.com>
2016-01-28 00:44:43 -0300

    Created dictionary with count cue phrases per document

--- main.py
+++ main.py
@@ -15,16 +15,15 @@ cue_phrases = [
     'hardly',
     'impossible',
     'The purpose',
-    'information'
+    'The information'
 ]
 
 # CP = Cue-phrase score,
 # CPS = Number of cue-phrases in the sentence,
 # CPD = Total number of cue-phrases in the document
 
-file = open('documents/8514')
+file = open('documents/9136')
 sentences = file.readlines()
-#print(sentences)
 
 # x = 'Meu nome é rodrigo bastos. Eu sou louco por computação. Meu nome Rodrigo Bastos'
 #
@@ -42,5 +41,8 @@ for sentence in sentences:
         count_cue_phrases += len(match)
     if n_cue_phrases > 0:
         count_cue_phrases_document[sentence] = n_cue_phrases
-print(count_cue_phrases_document)
+#print(count_cue_phrases_document)
+
+for item in count_cue_phrases_document:
+    print(item, count_cue_phrases_document[item])
 
